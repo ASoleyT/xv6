1c76aad49421693319b91d2ec6e04212870ba526
ASoleyT <developer.soleimanitabar@gmail.com>
2025-05-07 01:48:56 +0330

    initial logger

---
+++ kernel/custom_logger.c
@@ -0,0 +1,25 @@
+#include "types.h"
+#include "riscv.h"
+#include "logger.h"
+#include "defs.h"
+void log_message(int level, const char *message) {
+
+    if(level==1){
+
+
+
+    }else if(level==2){
+
+
+    }else if(level==3){
+
+
+    }else{
+
+        
+    }
+
+
+
+}
+
