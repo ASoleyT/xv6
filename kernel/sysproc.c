5675f7318cb27a13f80b47c29a26c5e6e463e547
ASoleyT <developer.soleimanitabar@gmail.com>
2025-07-12 18:03:47 +0330

    needed function for thread manupluation is added

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -114,3 +114,40 @@ int id;
 argint(0, &id);
 return jointhread(id);
 }
+
+
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+  argint(0, &n);
+  
+  if(n < 0)
+  n = 0;
+
+  acquire(&tickslock);
+  ticks0 = ticks;
+
+  if (myproc()->current_thread) {
+    release(&tickslock);
+    sleepthread(n, ticks0);
+    return 0;
+    }
+
+
+  while(ticks - ticks0 < n){
+
+    if(killed(myproc())){
+    release(&tickslock);
+    return -1;
+    }
+
+    sleep(&ticks, &tickslock);
+
+  }
+
+  release(&tickslock);
+  return 0;
+}
