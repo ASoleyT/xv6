85abcd82a276ca0935283daf81a11aefd56751ce
ASoleyT <developer.soleimanitabar@gmail.com>
2025-07-13 15:38:32 +0330

    changing the printing format in test file

--- user/threadtest.c
+++ user/threadtest.c
@@ -12,7 +12,7 @@ void *my_thread(void *arg) {
     uint64 number = (uint64)arg;
     for (int i = 0; i < 100; ++i) {
         number++;
-        printf("\n thread: %lu \n", number);
+        printf("thread:%lu \n", number);
     }
     return (void *)number;
 }
@@ -21,13 +21,13 @@ int main(int argc, char *argv[]) {
     int sp1[STACK_SIZE], sp2[STACK_SIZE], sp3[STACK_SIZE];
 
     int ta = thread(my_thread, sp1 + STACK_SIZE, (void *)100);
-    printf("NEW THREAD CREATED  %d\n", ta);
+    printf("NEW THREAD CREATED %d\n", ta);
 
     int tb = thread(my_thread, sp2 + STACK_SIZE, (void *)200);
-    printf("NEW THREAD CREATED  %d\n", tb);
+    printf("NEW THREAD CREATED %d\n", tb);
 
     int tc = thread(my_thread, sp3 + STACK_SIZE, (void *)300);
-    printf("NEW THREAD CREATED  %d\n", tc);
+    printf("NEW THREAD CREATED %d\n", tc);
 
     jointhread(ta);
     jointhread(tb);
